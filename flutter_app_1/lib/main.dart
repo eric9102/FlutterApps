c4847152945c7fc53438963fb5e5504687064d46
eric <rosylxf@gmail.com>
2019-10-11 18:14:41 +0800

    update

--- flutter_app_1/lib/main.dart
+++ flutter_app_1/lib/main.dart
@@ -85,36 +85,13 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -130,7 +107,12 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.red),//20 号红色粗体展示
             ),
 
-            Text.rich(
+           FloatingActionButton(onPressed: () => print('FloatingActionButton pressed'),child: Text('Btn'),),
+           FlatButton(onPressed: () => print('FlatButton pressed'),child: Text('Btn'),),
+           RaisedButton(onPressed: () => print('RaisedButton pressed'),child: Text('Btn'),),
+
+
+    Text.rich(
               TextSpan(
                   children: <TextSpan>[
                     TextSpan(text:'文本是视图系统中常见的控件，它用来显示一段特定样式的字符串，类似', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.red)), // 第 1 个片段，红色样式
@@ -150,6 +132,7 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
