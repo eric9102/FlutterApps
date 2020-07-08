6fdd5aee6bf263c42cf58fb9cb0d0a2dd7bd0bb0
le2m0vi <le2m0vi@gmail.com>
2020-07-08 21:36:52 +0800

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# FlutterApps
-Flutter demos
-add
--- flutter_app_examples/android/app/src/main/kotlin/headai/com/flutter_app_nonob/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package headai.com.flutter_app_nonob
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
