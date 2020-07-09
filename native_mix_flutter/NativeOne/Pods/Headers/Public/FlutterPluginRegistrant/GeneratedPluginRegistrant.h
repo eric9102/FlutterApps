d5ad485a0b80c27d9f1f2394e6dbf1d060793c56
lixuefeng <lixuefeng@citicpub.com>
2020-07-09 12:16:30 +0800

    update

---
+++ native_mix_flutter/NativeOne/Pods/Headers/Public/FlutterPluginRegistrant/GeneratedPluginRegistrant.h
@@ -0,0 +1,1 @@
+../../../../../flutter_one/.ios/Flutter/FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.h
---
+++ native_mix_flutter/NativeOne/Pods/Target Support Files/Pods-NativeOne/Pods-NativeOne-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NativeOne : NSObject
+@end
+@implementation PodsDummy_Pods_NativeOne
+@end
