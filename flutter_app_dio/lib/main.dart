1538afe9bae730260f17bec7697b56507371111f
le2m0vi <le2m0vi@gmail.com>
2020-07-11 02:00:18 +0800

    update

--- flutter_app_dio/lib/main.dart
+++ flutter_app_dio/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home'),
     );
   }
 }
@@ -27,46 +28,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  
-
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Dio _dio = Dio();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: FutureBuilder(
+              future:
+                  _dio.get("https://api.github.com/orgs/flutterchina/repos"),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                //请求完成
+                if (snapshot.connectionState == ConnectionState.done) {
+                  Response response = snapshot.data;
+                  //发生错误
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  }
+                  //请求成功，通过项目信息构建用于显示项目名称的ListView
+                  return ListView(
+                    children: response.data
+                        .map<Widget>(
+                            (e) => ListTile(title: Text(e["full_name"])))
+                        .toList(),
+                  );
+                }
+                //请求未完成时弹出loading
+                return CircularProgressIndicator();
+              }),
+        ));
   }
 }
