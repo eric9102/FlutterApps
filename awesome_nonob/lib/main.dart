ab0b2d23b5ae5295f37e386a816146b94dd8104e
le2m0vi <le2m0vi@gmail.com>
2020-07-18 18:54:38 +0800

    update

--- awesome_nonob/lib/main.dart
+++ awesome_nonob/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:awesome_nonob/TabPageFour.dart';
 import 'package:awesome_nonob/TabPageOne.dart';
 import 'package:awesome_nonob/TabPageThree.dart';
 import 'package:awesome_nonob/TabPageTwo.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,6 +16,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Colors.white,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -26,43 +29,79 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        body: TabBarView(
-          physics: NeverScrollableScrollPhysics(),
-          children: [
-            TabPageOne(),
-            TabPageTwo(),
-            TabPageThree(),
-            TabPageFour(),
-          ],
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(
-              icon: Icon(Icons.home),
-              text: "基础",
+    return defaultTargetPlatform == TargetPlatform.android
+        ? DefaultTabController(
+            length: 4,
+            child: Scaffold(
+              body: TabBarView(
+                physics: NeverScrollableScrollPhysics(),
+                children: [
+                  TabPageOne(),
+                  TabPageTwo(),
+                  TabPageThree(),
+                  TabPageFour(),
+                ],
+              ),
+              bottomNavigationBar: TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.home),
+                    text: "基础",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.rss_feed),
+                    text: "进阶",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.access_time),
+                    text: "三方",
+                  ),
+                  Tab(
+                    icon: Icon(Icons.person),
+                    text: "整合",
+                  )
+                ],
+                unselectedLabelColor: Colors.blueGrey,
+                labelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorColor: Colors.red,
+              ),
             ),
-            Tab(
-              icon: Icon(Icons.rss_feed),
-              text: "进阶",
-            ),
-            Tab(
-              icon: Icon(Icons.access_time),
-              text: "三方",
-            ),
-            Tab(
-              icon: Icon(Icons.person),
-              text: "整合",
-            )
-          ],
-          unselectedLabelColor: Colors.blueGrey,
-          labelColor: Colors.blue,
-          indicatorSize: TabBarIndicatorSize.label,
-          indicatorColor: Colors.red,
-        ),
-      ),
-    );
+          )
+        : CupertinoTabScaffold(
+            tabBar: CupertinoTabBar(items: [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), title: Text("基础")),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.battery_charging),
+                  title: Text("进阶")),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.time), title: Text("三方")),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.person), title: Text("整合"))
+            ]),
+            tabBuilder: (context, index) {
+              return CupertinoTabView(
+                builder: (context) {
+                  switch (index) {
+                    case 0:
+                      return TabPageOne();
+                      break;
+                    case 1:
+                      return TabPageTwo();
+                      break;
+                    case 2:
+                      return TabPageThree();
+                      break;
+                    case 3:
+                      return TabPageFour();
+                      break;
+                    default:
+                      return Container();
+                  }
+                },
+              );
+            },
+          );
   }
 }
