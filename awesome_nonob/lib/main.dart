669a7622c673c83adb92ea584f91b5fa73b020ed
le2m0vi <le2m0vi@gmail.com>
2020-07-18 19:58:51 +0800

    update

--- awesome_nonob/lib/main.dart
+++ awesome_nonob/lib/main.dart
@@ -29,79 +29,80 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return defaultTargetPlatform == TargetPlatform.android
-        ? DefaultTabController(
-            length: 4,
-            child: Scaffold(
-              body: TabBarView(
-                physics: NeverScrollableScrollPhysics(),
-                children: [
-                  TabPageOne(),
-                  TabPageTwo(),
-                  TabPageThree(),
-                  TabPageFour(),
-                ],
+    if (defaultTargetPlatform == TargetPlatform.android) {
+      return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: [
+              TabPageOne(),
+              TabPageTwo(),
+              TabPageThree(),
+              TabPageFour(),
+            ],
+          ),
+          bottomNavigationBar: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.home),
+                text: "基础",
               ),
-              bottomNavigationBar: TabBar(
-                tabs: [
-                  Tab(
-                    icon: Icon(Icons.home),
-                    text: "基础",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.rss_feed),
-                    text: "进阶",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.access_time),
-                    text: "三方",
-                  ),
-                  Tab(
-                    icon: Icon(Icons.person),
-                    text: "整合",
-                  )
-                ],
-                unselectedLabelColor: Colors.blueGrey,
-                labelColor: Colors.blue,
-                indicatorSize: TabBarIndicatorSize.label,
-                indicatorColor: Colors.red,
+              Tab(
+                icon: Icon(Icons.rss_feed),
+                text: "进阶",
               ),
-            ),
-          )
-        : CupertinoTabScaffold(
-            tabBar: CupertinoTabBar(items: [
-              BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.home), title: Text("基础")),
-              BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.battery_charging),
-                  title: Text("进阶")),
-              BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.time), title: Text("三方")),
-              BottomNavigationBarItem(
-                  icon: Icon(CupertinoIcons.person), title: Text("整合"))
-            ]),
-            tabBuilder: (context, index) {
-              return CupertinoTabView(
-                builder: (context) {
-                  switch (index) {
-                    case 0:
-                      return TabPageOne();
-                      break;
-                    case 1:
-                      return TabPageTwo();
-                      break;
-                    case 2:
-                      return TabPageThree();
-                      break;
-                    case 3:
-                      return TabPageFour();
-                      break;
-                    default:
-                      return Container();
-                  }
-                },
-              );
+              Tab(
+                icon: Icon(Icons.access_time),
+                text: "三方",
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+                text: "整合",
+              )
+            ],
+            unselectedLabelColor: Colors.blueGrey,
+            labelColor: Colors.blue,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Colors.red,
+          ),
+        ),
+      );
+    } else {
+      return CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(items: [
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), title: Text("基础-iOS")),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.battery_charging), title: Text("进阶")),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.time), title: Text("三方")),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person), title: Text("整合"))
+        ]),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(
+            builder: (context) {
+              switch (index) {
+                case 0:
+                  return TabPageOne();
+                  break;
+                case 1:
+                  return TabPageTwo();
+                  break;
+                case 2:
+                  return TabPageThree();
+                  break;
+                case 3:
+                  return TabPageFour();
+                  break;
+                default:
+                  return Container();
+              }
             },
           );
+        },
+      );
+    }
   }
 }
