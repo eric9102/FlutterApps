27931fd443f5336724095879015f537fa4afbac8
le2m0vi <le2m0vi@gmail.com>
2020-07-26 17:09:39 +0800

    update

--- awesome_nonob/lib/BasicWidgets/FirstFlutterApp.dart
+++ awesome_nonob/lib/BasicWidgets/FirstFlutterApp.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 class RandomWords extends StatefulWidget {
   RandomWords({Key key}) : super(key: key);
@@ -10,8 +12,16 @@ class RandomWords extends StatefulWidget {
 class _RandomWordsState extends State<RandomWords> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+    final wordPair = WordPair.random();
+    // return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: PreferredSize(
+          child: AppBar(
+            title: Text(wordPair.asPascalCase),
+            elevation: 0,
+          ),
+          preferredSize: Size.fromHeight(44)),
+      body: Text(wordPair.asPascalCase),
     );
   }
 }
